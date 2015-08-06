8d391b2b07b630d58f3c00e4f49ff1a62b3514cf
宫辰 <chen.gong@ctgtmo.com>
2015-08-06 10:58:21 +0800

    Initial Commit

---
+++ GCCalenderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCCalenderDemo
+//
+//  Created by 辰 宫 on 15/8/6.
+//  Copyright © 2015年 辰 宫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCCalenderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCCalenderDemo
+//
+//  Created by 辰 宫 on 15/8/6.
+//  Copyright © 2015年 辰 宫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
